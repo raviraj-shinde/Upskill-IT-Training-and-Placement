c00b40682ea88919d4a99c0be03013ff34afab17
Raviraj <ravishinde96k@gmail.com>
2026-01-12 22:02:57 +0530

    Array

---
+++ Practicals/JS/Array/script.js
@@ -0,0 +1,79 @@
+function printToOutput(data) {
+  let outputDiv = document.getElementById("output");
+  let div = document.createElement("div");
+  div.innerText = data;
+  outputDiv.appendChild(div);
+}
+
+function clearOutput() {
+  const outputDiv = document.getElementById("output");
+  outputDiv.textContent = "";
+}
+
+/**************************** * CODE * *************************************/
+
+let arr = [];
+
+for (let i = 0; i < 4; i++) {
+  let num = parseInt(prompt("Enter Num " + (i + 1) + " :- "));
+  arr[i] = num;
+}
+
+/**************************** 2 ***************************************/
+
+function showArrEle() {
+  for (let i = 0; i < arr.length; i++) {
+    printToOutput("Value at " + i + " Index : " + arr[i]);
+  }
+}
+
+showArrEle();
+
+/****************************** 3 *************************************/
+
+//Reverse an Array
+
+for (let i = 0, j = arr.length - 1; i < j; i++, j--) {
+  arr[i] = arr[i] ^ arr[j];
+  arr[j] = arr[i] ^ arr[j];
+  arr[i] = arr[i] ^ arr[j];
+}
+
+printToOutput("Reversed Array" + arr);
+
+/****************************** 4 *************************************/
+
+let arr2 = [
+  1,
+  "1",
+  { num: "zero" },
+  () => {
+    console.log("hello");
+  },
+];
+
+printToOutput(arr2);
+
+/***************************** 5 **************************************/
+
+let sum = arr.reduce((sum, num) => sum + num, 0);
+printToOutput("Sum of inpput arr:- " + sum);
+
+/***************************** 6 **************************************/
+
+let arr3 = [
+  [1, 2],
+  [2, 4]
+];
+
+let data = "[ ";
+for (let i = 0; i < arr3.length; i++) {
+  data += "[ ";
+  for (let j = 0; j < arr3[i].length; j++) {
+    data += arr3[i][j] + ",";
+  }
+  data += "], ";
+}
+printToOutput(data + " ]");
+
+/***************************** Transpose 2D Array**************************************/
