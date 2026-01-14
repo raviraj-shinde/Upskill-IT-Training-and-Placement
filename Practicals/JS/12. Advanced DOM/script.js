f12e0e8021fe4c2e22347b5706bb84d64367688a
Raviraj <ravishinde96k@gmail.com>
2026-01-14 17:11:28 +0530

    Password Hide and Show

---
+++ Practicals/JS/12. Advanced DOM/script.js
@@ -0,0 +1,32 @@
+function printToOutput(data) {
+  let outputDiv = document.getElementById("output");
+  let div = document.createElement("div");
+  div.innerText = data;
+  outputDiv.appendChild(div);
+}
+
+function clearOutput() {
+  const outputDiv = document.getElementById("output");
+  outputDiv.textContent = "";
+}
+
+/*******************************************************************/
+
+
+let inp = document.getElementById("inp-pass");
+let btn = document.getElementById("btn-toggle");
+let showPass = 0;
+
+function toggleShow () {
+  //toggle 1-> show,  0 -> not
+  console.log("working");
+  if(showPass === 0){
+    inp.type = "password"; //hide
+    btn.innerText = "👁️ Show";
+    showPass = 1;
+  } else{
+    inp.type = "text"; //show
+    btn.innerText = "🔑 Hide";
+    showPass = 0;
+  }
+}
