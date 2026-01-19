057ef1681df751404c9b49c9b8b2eb0184377298
Raviraj <ravishinde96k@gmail.com>
2026-01-19 16:47:42 +0530

    Renamed practical folders

---
+++ Practicals/3. JQuery/1. 📌 Output Layout/script.js
@@ -0,0 +1,13 @@
+function printToOutput(data) {
+  let outputDiv = document.getElementById("output");
+  let div = document.createElement("div");
+  div.innerText = data;
+  outputDiv.appendChild(div);
+}
+
+function clearOutput() {
+  const outputDiv = document.getElementById("output");
+  outputDiv.textContent = "";
+}
+
+/*******************************************************************/
